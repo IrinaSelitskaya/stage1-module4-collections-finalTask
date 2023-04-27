04d774abeaa89396565604c74fa91dd1fad59b28
IrinaSelitskaya <silya@tut.by>
2023-04-27 14:50:41 +0300

    Updated LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -7,8 +7,8 @@ public class LessonsGetter {
         Set<String>set=new HashSet<>();
         List<List<String>>arrayList=new ArrayList<>(timetable.values());
         for (List<String>list:arrayList){
-            for (int i=0;i<= list.size();i++){
-                set.add(list.get(i));
+            for (String s:list){
+                set.add(s);
             }
         }
 return set;
